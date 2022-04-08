750972a6096ff4708d59557bc69113b3846bb36b
m.m.velzel@student.utwente.nl <m.m.velzel@student.utwente.nl>
2022-04-08 22:25:02 +0200

    moves to movement namespace

---
+++ Assets/BUK/Scripts/Motion/Roll.cs
@@ -0,0 +1,51 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+namespace Buk.Motion
+{
+  public class Roll : MonoBehaviour {
+
+    private Rigidbody body;
+    private float initialAngularDrag;
+
+    public InputAction gas;
+    public InputAction brake;
+    public float torque = 2.0f;
+    public Camera camera;
+    public float maximumAngularVelocity = 25f;
+
+    public void Awake() {
+      body = GetComponent<Rigidbody>();
+      body.maxAngularVelocity = maximumAngularVelocity;
+      var initialAngularDrag = body.angularDrag;
+
+      // Enable controls
+      gas.Enable();
+      brake.Enable();
+
+      // Handle braking
+      brake.performed += Brake;
+      brake.canceled += Cancel;
+    }
+
+    // Increase drag when brake is pressed
+    void Brake(InputAction.CallbackContext _) => body.angularDrag = initialAngularDrag * torque;
+
+    // Reset drag when brake is released
+    void Cancel(InputAction.CallbackContext _) => body.angularDrag = initialAngularDrag;
+
+    public void Update() {
+      // Read gas input
+      var throttle = gas.ReadValue<float>();
+      if (throttle != 0) {
+        // Add rotational force to spin ball in the direction the camera is looking.
+        body.AddTorque(camera.transform.right * throttle * torque, ForceMode.Force);
+      }
+    }
+
+    public void OnDestroy() {
+      brake.performed -= Brake;
+      brake.canceled -= Cancel;
+    }
+  }
+}
