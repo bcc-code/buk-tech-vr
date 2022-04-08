57f08aaae171c246f8f0be3d19af9fde9a315e70
m.m.velzel@student.utwente.nl <m.m.velzel@student.utwente.nl>
2022-04-08 22:25:49 +0200

    adds script for enabling and disabling vr

---
+++ Assets/BUK/Scripts/VrEnabled.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.Management;
+
+namespace Buk
+{
+    public class VrEnabled : MonoBehaviour
+    {
+        public bool isVREnabled;
+    
+        void Start()
+        {
+            if (isVREnabled)
+                StartCoroutine(StartXRCoroutine());
+            else
+                StopXR();
+        }
+    
+        public IEnumerator StartXRCoroutine()
+        {
+            Debug.Log("Initializing XR...");
+            yield return XRGeneralSettings.Instance.Manager.InitializeLoader();
+    
+            if (XRGeneralSettings.Instance.Manager.activeLoader == null)
+            {
+                Debug.LogError("Initializing XR Failed. Check Editor or Player log for details.");
+            }
+            else
+            {
+                Debug.Log("Starting XR...");
+                XRGeneralSettings.Instance.Manager.StartSubsystems();
+            }
+        }
+    
+        void StopXR()
+        {
+            if (XRGeneralSettings.Instance.Manager.isInitializationComplete)
+            {
+                Debug.Log("Stopping XR...");
+        
+                XRGeneralSettings.Instance.Manager.StopSubsystems();
+                XRGeneralSettings.Instance.Manager.DeinitializeLoader();
+                Debug.Log("XR stopped completely.");
+            }
+        }
+    }
+}
